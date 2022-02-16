fe93bc09f9cca98e8db5990471ae1f4caa902c00
Jimmy Chen <weijchen0919@gmail.com>
2022-02-16 18:40:53 -0500

    test code

--- Assets/DraggableObjAdv.cs
+++ Assets/DraggableObjAdv.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,18 @@ public class DraggableObjAdv : MonoBehaviour
     private float width;
     private float height;
 
+    private bool touched = false;
+    private bool dragging = false;
+    private bool hasGravityChg = false;
+    private Rigidbody _rigidbody;
+
     void Awake()
     {
         width = (float)Screen.width / 2.0f;
         height = (float)Screen.height / 2.0f;
 
         // Position used for the cube.
-        position = new Vector3(0.0f, 0.0f, 0.0f);
+        // position = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     void OnGUI()
@@ -25,44 +31,82 @@ public class DraggableObjAdv : MonoBehaviour
 
         GUI.Label(new Rect(20, 20, width, height * 0.25f),
             "x = " + position.x.ToString("f2") +
-            ", y = " + position.y.ToString("f2"));
+            ", y = " + position.y.ToString("f2") + 
+            ", hasTouched = " + touched +
+            ", hasDragged = " + dragging +
+            ", gravity changed = " + hasGravityChg);
     }
 
-    void Update()
+    private void Start()
     {
-        // Handle screen touches.
-        if (Input.touchCount > 0)
+        _rigidbody = transform.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.touchCount != 1)
         {
-            Touch touch = Input.GetTouch(0);
+            dragging = false;
+            touched = false;
+            SetFreeProperties(_rigidbody);
+            return;
+        }
 
-            // Move the cube if the screen has the finger moving.
-            if (touch.phase == TouchPhase.Moved)
+        Touch touch = Input.touches[0];
+        // Vector3 pos = touch.position;
+        
+        // Handle screen touches
+        
+        // Move the cube if the screen has the finger moving.
+        if (touch.phase == TouchPhase.Moved)
+        {
+            Vector2 pos = touch.position;
+            pos.x = (pos.x - width) / width;
+            pos.y = (pos.y - height) / height;
+            position = new Vector3(-pos.x, pos.y, 0.0f);
+            SetDraggingProperties(_rigidbody);
+
+            // Position the cube.
+            transform.position = position;
+        }
+
+        if (Input.touchCount == 2)
+        {
+            touch = Input.GetTouch(1);
+
+            if (touch.phase == TouchPhase.Began)
             {
-                Vector2 pos = touch.position;
-                pos.x = (pos.x - width) / width;
-                pos.y = (pos.y - height) / height;
-                position = new Vector3(-pos.x, pos.y, 0.0f);
-
-                // Position the cube.
-                transform.position = position;
+                // Halve the size of the cube.
+                transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
             }
 
-            if (Input.touchCount == 2)
+            if (touch.phase == TouchPhase.Ended)
             {
-                touch = Input.GetTouch(1);
-
-                if (touch.phase == TouchPhase.Began)
-                {
-                    // Halve the size of the cube.
-                    transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
-                }
-
-                if (touch.phase == TouchPhase.Ended)
-                {
-                    // Restore the regular size of the cube.
-                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                }
+                // Restore the regular size of the cube.
+                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             }
         }
+    }
+    
+    /*
+     * Set the physical situation when objects were being dragged
+     */
+    private void SetDraggingProperties(Rigidbody rb)
+    {
+        rb.isKinematic = false;
+        rb.useGravity = false;
+        rb.drag = 20;
+        hasGravityChg = true;
+    }
+
+    /*
+     * Let the objects' physic goes back to normal
+     */
+    private void SetFreeProperties(Rigidbody rb)
+    {
+        rb.isKinematic = true;
+        rb.useGravity = true;
+        rb.drag = 5;
+        hasGravityChg = false;
     }
 }
