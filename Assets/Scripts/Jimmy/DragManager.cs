0d990424430fd770e8319053d1042c66f5b52445
jimmyweicc <weijchen0919@gmail.com>
2022-02-18 00:47:03 -0500

    finish drag and drop function

---
+++ Assets/Scripts/Jimmy/DragManager.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class DragManager : MonoBehaviour
+{
+    [SerializeField] private float mouseDragPhysicsSpeed = 10.0f;
+    [SerializeField] private float mouseDragSpeed = 1.0f;
+    public delegate void StartTouchEvent(Vector2 position, float time);
+
+    public event StartTouchEvent OnStartTouch;
+
+    public delegate void EndTouchEvent(Vector2 position, float time);
+
+    public event EndTouchEvent OnEndTouch;
+    private TouchControls _touchControls;
+    private Camera mainCamera;
+    private Vector3 velocity = Vector3.zero;
+    private WaitForFixedUpdate _waitForFixedUpdate = new WaitForFixedUpdate();
+
+    private float width;
+    private float height;
+    private bool isEnable = false;
+    private bool isPress = false;
+    private bool isTouch = false;
+    private bool isDrag = false;
+    private float posX = .0f;
+    private float posY = .0f;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        _touchControls = new TouchControls();
+        width = Screen.width / 2.0f;
+        height = Screen.height / 2.0f;
+    }
+
+    private void OnGUI()
+    {
+        // Compute a fontSize based on the size of the screen width.
+        GUI.skin.label.fontSize = (int) (Screen.width / 50.0f);
+
+        GUI.Label(new Rect(20, 20, width, height * 0.25f),
+            "pos(X) = " + posX + " pos(Y) = " + posY + 
+            "enabled = " + isEnable + " pressed = " + isPress + " touched = " + isTouch + " drag = " + isDrag);
+    }
+
+    private void Start()
+    {
+        _touchControls.Touch.TouchPress.started += ctx => StartTouch(ctx);
+        _touchControls.Touch.TouchPress.canceled += ctx => EndTouch(ctx);
+    }
+
+    private void OnEnable()
+    {
+        _touchControls.Enable();
+        isEnable = true;
+    }
+
+    private void OnDisable()
+    {
+        _touchControls.Disable();
+        isEnable = false;
+    }
+
+    void StartTouch(InputAction.CallbackContext context)
+    {
+        Vector2 touchPosition = _touchControls.Touch.TouchPosition.ReadValue<Vector2>();
+        Debug.Log("Touch started " + touchPosition);
+        posX = touchPosition.x;
+        posY = touchPosition.y;
+        if (OnStartTouch != null)
+            OnStartTouch(touchPosition, (float) context.startTime);
+
+        Ray ray = mainCamera.ScreenPointToRay(touchPosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            isTouch = true;
+            if (hit.collider != null && (hit.collider.gameObject.CompareTag("Draggable")))
+            {
+                Debug.Log("dragging somthing");
+                isDrag = true;
+                StartCoroutine(DragUpdate(hit.collider.gameObject));
+            }
+            else
+            {
+                isDrag = false;
+            }
+        }
+
+        isTouch = false;
+        isDrag = false;
+    }
+
+    void EndTouch(InputAction.CallbackContext context)
+    {
+        Debug.Log("Touch ended" + _touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+        if (OnEndTouch != null)
+            OnEndTouch(_touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float) context.time);
+    }
+
+    private IEnumerator DragUpdate(GameObject clickObj)
+    {
+        float initialDistance = Vector3.Distance(clickObj.transform.position, mainCamera.transform.position);
+        clickObj.TryGetComponent<Rigidbody>(out var rb);
+        while (_touchControls.Touch.TouchPress.ReadValue<float>() != 0)
+        {
+            Debug.Log("updating drag position");
+            Ray ray = mainCamera.ScreenPointToRay(_touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+            if (rb != null)
+            {
+                Vector3 direction = ray.GetPoint(initialDistance) - clickObj.transform.position;
+                rb.velocity = direction * mouseDragPhysicsSpeed;
+                yield return _waitForFixedUpdate;
+            }
+            else
+            {
+                clickObj.transform.position = Vector3.SmoothDamp(clickObj.transform.position,
+                    ray.GetPoint(initialDistance), ref velocity, mouseDragSpeed);
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Jimmy/TouchControls.cs
@@ -0,0 +1,190 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Jimmy/TouchControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @TouchControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @TouchControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""TouchControls"",
+    ""maps"": [
+        {
+            ""name"": ""Touch"",
+            ""id"": ""24e5c5f0-2615-4175-a863-1f9690003581"",
+            ""actions"": [
+                {
+                    ""name"": ""TouchInput"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""e63dddeb-313b-4f5c-a72b-7408fa1aefe3"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""TouchPress"",
+                    ""type"": ""Button"",
+                    ""id"": ""ab5c48b5-f58e-4b4c-acb0-8f0b52a7af69"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""TouchPosition"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""3b68e1d5-d473-4292-b6c1-6ec4e8230220"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""e96408c2-14d8-4ec8-a244-289a919c6d92"",
+                    ""path"": ""<Touchscreen>/primaryTouch"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""TouchInput"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9a293a8f-7b17-41d0-9911-d7c8fff11738"",
+                    ""path"": ""<Touchscreen>/primaryTouch/press"",
+                    ""interactions"": ""Press(behavior=2)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""TouchPress"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""297aae90-c5cb-4e73-916a-e14c0ad4b6bb"",
+                    ""path"": ""<Touchscreen>/primaryTouch/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""TouchPosition"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Touch
+        m_Touch = asset.FindActionMap("Touch", throwIfNotFound: true);
+        m_Touch_TouchInput = m_Touch.FindAction("TouchInput", throwIfNotFound: true);
+        m_Touch_TouchPress = m_Touch.FindAction("TouchPress", throwIfNotFound: true);
+        m_Touch_TouchPosition = m_Touch.FindAction("TouchPosition", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Touch
+    private readonly InputActionMap m_Touch;
+    private ITouchActions m_TouchActionsCallbackInterface;
+    private readonly InputAction m_Touch_TouchInput;
+    private readonly InputAction m_Touch_TouchPress;
+    private readonly InputAction m_Touch_TouchPosition;
+    public struct TouchActions
+    {
+        private @TouchControls m_Wrapper;
+        public TouchActions(@TouchControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @TouchInput => m_Wrapper.m_Touch_TouchInput;
+        public InputAction @TouchPress => m_Wrapper.m_Touch_TouchPress;
+        public InputAction @TouchPosition => m_Wrapper.m_Touch_TouchPosition;
+        public InputActionMap Get() { return m_Wrapper.m_Touch; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(TouchActions set) { return set.Get(); }
+        public void SetCallbacks(ITouchActions instance)
+        {
+            if (m_Wrapper.m_TouchActionsCallbackInterface != null)
+            {
+                @TouchInput.started -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchInput;
+                @TouchInput.performed -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchInput;
+                @TouchInput.canceled -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchInput;
+                @TouchPress.started -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchPress;
+                @TouchPress.performed -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchPress;
+                @TouchPress.canceled -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchPress;
+                @TouchPosition.started -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchPosition;
+                @TouchPosition.performed -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchPosition;
+                @TouchPosition.canceled -= m_Wrapper.m_TouchActionsCallbackInterface.OnTouchPosition;
+            }
+            m_Wrapper.m_TouchActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @TouchInput.started += instance.OnTouchInput;
+                @TouchInput.performed += instance.OnTouchInput;
+                @TouchInput.canceled += instance.OnTouchInput;
+                @TouchPress.started += instance.OnTouchPress;
+                @TouchPress.performed += instance.OnTouchPress;
+                @TouchPress.canceled += instance.OnTouchPress;
+                @TouchPosition.started += instance.OnTouchPosition;
+                @TouchPosition.performed += instance.OnTouchPosition;
+                @TouchPosition.canceled += instance.OnTouchPosition;
+            }
+        }
+    }
+    public TouchActions @Touch => new TouchActions(this);
+    public interface ITouchActions
+    {
+        void OnTouchInput(InputAction.CallbackContext context);
+        void OnTouchPress(InputAction.CallbackContext context);
+        void OnTouchPosition(InputAction.CallbackContext context);
+    }
+}
